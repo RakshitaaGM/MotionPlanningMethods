8f0da80cf71d8dc76c23f97c7ecf31cf46174fce
RakshitaaGM <rakshitaagm@gmail.com>
2023-05-21 17:00:57 -0500

    added maps

--- src/drive.cpp
+++ src/drive.cpp
@@ -1,61 +1,31 @@
 #include<iostream>
 #include<ros/ros.h>
+#include<std_msgs/Header.h>
 #include <geometry_msgs/Twist.h>
+#include<nav_msgs/OccupancyGrid.h>
+#include "Astar.h"
 
+void mapCallback(const nav_msgs::OccupancyGrid& msg)
+{
+    std_msgs::Header header = msg.header;
+    nav_msgs::MapMetaData mapInfo = msg.info;
+    for(uint x = 0; x < mapInfo.width ; x++)
+    {
+        for(uint y = 0; y < mapInfo.height; y++)
+        {
+            std::cout << int(msg.data[x + mapInfo.width * y]);
+        }
+    }
+    
+
+}
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "moveRobot");
+    ros::init(argc, argv, "astar");
     ros::NodeHandle n;
-    ros::Publisher pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 10);
-    ros::Rate loop_rate(50);
-
-    double linear_speed=0.2;
-    double goal_distance=1.0;
-    double linear_duration=goal_distance/linear_speed;
-    double angular_speed=1.0;
-    double goal_angle=0.0;
-    double angular_duration=0.0;
-    geometry_msgs::Twist twist_msg;
-    twist_msg.linear.x=twist_msg.linear.y=twist_msg.linear.z=0;
-    twist_msg.angular.x=twist_msg.angular.y=twist_msg.angular.z=0;
-
-    while(ros::ok())
-    {
-        // twist_msg.linear.x=linear_speed;
-        // int ticks=static_cast<int>(linear_duration*rate);
-        // for(int j=0;j<ticks;j++)
-        // {
-        //     cmd_vel_pub.publish(twist_msg);
-        //     loop_rate.sleep();
-        // }
-
-        // twist_msg.linear.x=0;
-        // cmd_vel_pub.publish(twist_msg);
-        // ros::Duration(1).sleep();
-
-        // twist_msg.angular.z=angular_speed;
-        // ticks=static_cast<int>(goal_angle*rate);
-        // for(int j=0;j<ticks;j++)
-        // {
-        //     cmd_vel_pub.publish(twist_msg);
-        //     loop_rate.sleep();
-        // }
-
-        // twist_msg.angular.z=0;
-        // cmd_vel_pub.publish(twist_msg);
-        // ros::Duration(1).sleep();
-        geometry_msgs::Twist msg;
-        msg.linear.x = 0.2;
-        pub.publish(msg);
-        loop_rate.sleep();
-
-    }
-
-    // ttwist_msg.linear.x=0;
-    // twist_msg.angular.z=0;
-    // cmd_vel_pub.publish(twist_msg);
-    // ros::Duration(1).sleep();
+    ros::Subscriber mapSub = n.subscribe("map", 1000, mapCallback);
+    ros::spin();
 
     return 0;
 }
